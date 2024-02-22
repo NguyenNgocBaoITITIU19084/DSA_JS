dac058397f06d68444a4ad700ab16e14ddd66ab6
NguyenNgocBaoITITIU19084 <ititiu19084@student.hcmiu.edu.vn>
2024-02-22 10:05:57 +0700

    accessor function in JS

--- Array/Accessor_Function.js
+++ Array/Accessor_Function.js
@@ -1,5 +1,40 @@
 console.log(
   "===============================Searching for a Value==============================="
 );
+const names = [
+  "Kun",
+  "Bao",
+  "Ngan",
+  "Tien",
+  "Phong",
+  "Kun",
+  "Bao",
+  "Ngan",
+  "Tien",
+  "Phong",
+];
 console.log("*****indexOf*****");
+// indexOf will return the index of first value in array if it was founded and return -1 if it was not founded
+const searchingWithIndexOf = (keyword) => {
+  const index = names.indexOf(keyword);
+  return index;
+};
+console.log("Index of keyword with index of is:", searchingWithIndexOf("Ngan"));
+console.log(
+  "Index of keyword with index of is:",
+  searchingWithLastIndexOf("Ngannn")
+);
 console.log("*****lastIndexOf*****");
+// indexOf will return the index of last value in array if it was founded and return -1 if it was not founded
+const searchingWithLastIndexOf = (keyword) => {
+  const index = names.lastIndexOf(keyword);
+  return index;
+};
+console.log(
+  "Index of keyword with index of is:",
+  searchingWithLastIndexOf("Ngan")
+);
+console.log(
+  "Index of keyword with index of is:",
+  searchingWithLastIndexOf("Ngannn")
+);
